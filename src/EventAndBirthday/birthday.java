bb1eb83e0a8064335adb776b63ec59621d2ff2a1
ibrahem <habboushebraheem@gmail.com>
2024-09-07 19:57:07 +0300

    update

--- src/EventAndBirthday/birthday.java
+++ src/EventAndBirthday/birthday.java
@@ -9,14 +9,14 @@ import java.util.Scanner;
 public class birthday {
 
 	public static void main(String[] args) {
-Scanner scanner=new Scanner(System.in);
-System.out.println("inter your date of birthday :");
-String userbirthday=scanner.nextLine();
-DateTimeFormatter formatter=DateTimeFormatter.ofPattern("yyyy/MM/dd");
-LocalDate birthdayDate=LocalDate.parse(userbirthday,formatter);
-LocalDate todayDate=LocalDate.now();
-Period agePeriod=Period.between(birthdayDate, todayDate);
-System.out.println(agePeriod);
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("inter your date of birthday :");
+		String userbirthday = scanner.nextLine();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+		LocalDate birthdayDate = LocalDate.parse(userbirthday, formatter);
+		LocalDate todayDate = LocalDate.now();
+		Period agePeriod = Period.between(birthdayDate, todayDate);
+		System.out.println(agePeriod);
 	}
 
 }
